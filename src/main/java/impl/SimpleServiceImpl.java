1f288fe3cc6c313a7e16a7379ef7de20dd4f71b5
RUP <roma.ulina.tif418@polban.ac.id>
2020-05-06 17:24:54 +0700

    Update SimpleServiceImpl.java
    Works :)

--- src/main/java/impl/SimpleServiceImpl.java
+++ src/main/java/impl/SimpleServiceImpl.java
@@ -1,12 +1,11 @@
 package impl;
 
 import data.mahasiswa.SimpleService;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.InsertManyOptions;
 import com.mongodb.quickstart.models.Mahasiswa;
 
 import org.bson.Document;
@@ -23,32 +22,36 @@ import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class SimpleServiceImpl implements SimpleService {
-	MongoDatabase dataMahasiswaDB;
-	MongoCollection<Document> MahasiswaCollection;
-	public SimpleServiceImpl(){
+ MongoDatabase database;
+ MongoCollection<Mahasiswa> mahasiswaCollection;
+ public SimpleServiceImpl() {
+ MongoCredential credential; 
+ credential = MongoCredential.createCredential("sampleUser", "mahasiswadb", "password".toCharArray()); 
+ System.out.println("--Connected to the database successfully--");  
 
-	 Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
-        try (MongoClient mongoClient = MongoClients.create("mongodb+srv://uli:231407Jerapah99.@firstcluster-6mqrj.mongodb.net/test")) {
-        	
-            dataMahasiswaDB = mongoClient.getDatabase("datamahasiswa");
-            MahasiswaCollection = dataMahasiswaDB.getCollection("mahasiswaCollection");
+ CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
+         fromProviders(PojoCodecProvider.builder().automatic(true).build()));  
+ MongoClient mongo = new MongoClient("localhost", MongoClientOptions.builder().codecRegistry(pojoCodecRegistry).build());
+ 
+ // Accessing the database 
+ database = mongo.getDatabase("mahasiswadb"); 
+ mahasiswaCollection = database.getCollection("mahasiswaCollection", Mahasiswa.class);
+ database = database.withCodecRegistry(pojoCodecRegistry);
+ }
 
-        }
-	}
-
-	public boolean createMahasiswa(String nim,String nama,int semester, String jurusan, String alamat) {
-		String idMahasiswa = new ObjectId().toString();
-		try {
-			//Mahasiswa mahasiswa = new Mahasiswa(idMahasiswa, nim, nama, semester,jurusan, alamat);
-			//MahasiswaCollection.insertOne(mahasiswa);
-			System.out.println("[Account Created]");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}	
-		
-		return true;
-	}
+ public boolean createMahasiswa(String nim,String nama,int semester, String jurusan, String alamat) {
+  String idMahasiswa = new ObjectId().toString();
+  try {
+   Mahasiswa mahasiswa = new Mahasiswa(idMahasiswa, nim, nama, semester,jurusan, alamat);
+   mahasiswaCollection.insertOne(mahasiswa);
+   System.out.println("[Account Created]");
+  } catch (Exception e) {
+   e.printStackTrace();
+   return false;
+  } 
+  
+  return true;
+ }
     
 
 
